835be458f3f9a52fd01ba2259f87d0dc6f7dfcc1
songlinlin <songlinlin@lifesay.cn>
2017-01-11 22:52:38 +0800

    初始化项目文件

---
+++ WebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebView
+//
+//  Created by liuzhike on 17/1/11.
+//  Copyright © 2017年 王建业. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WebView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WebView
+//
+//  Created by liuzhike on 17/1/11.
+//  Copyright © 2017年 王建业. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
